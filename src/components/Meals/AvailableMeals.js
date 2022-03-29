2e5bfba5d1caca51b713f50f635093387f89d25e
Teju1919 <tbodduluri@aligntech.com>
2022-03-29 17:51:21 +0530

    Initial coment

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,54 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Pizza',
+    description: 'Finest meat and veggies',
+    price: 22.99,
+    img: 'pizza.svg'
+  },
+  {
+    id: 'm4',
+    name: 'Soups',
+    description: 'Healthy...and Tasty...',
+    price: 18.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+      img={meal.svg}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
